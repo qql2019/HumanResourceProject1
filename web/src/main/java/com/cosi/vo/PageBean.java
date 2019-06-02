678817e883f767c83021bde0659d2a860fe2c39b
qql <2870628170@qq.com>
2019-06-02 14:45:20 +0800

    user添加tostring

--- web/src/main/java/com/cosi/vo/PageBean.java
+++ web/src/main/java/com/cosi/vo/PageBean.java
@@ -25,7 +25,12 @@ public class PageBean {
 	public void setRows(int rows) {
 		this.rows = rows;
 	}
-	
-	
 
+	@Override
+	public String toString() {
+		return "PageBean{" +
+				"page=" + page +
+				", rows=" + rows +
+				'}';
+	}
 }
